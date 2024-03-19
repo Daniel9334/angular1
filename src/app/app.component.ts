76a50f38d4c15189caeaea3b8c74784f84c4fb41
Daniel9334 <daniel.banach.db@gmail.com>
2024-03-19 21:45:01 +0100

    Zmiany7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NgxRaceModule } from 'ngx-race';
 import { FormsModule } from '@angular/forms';
 import { GameComponent } from './game/game.component';
 import { IntroComponent } from './intro/intro.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgxRaceModule, FormsModule, GameComponent, IntroComponent],
+  imports: [CommonModule, NgxRaceModule, FormsModule, GameComponent, IntroComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -20,14 +21,19 @@ export class AppComponent {
   points: number = 0;
   timePlayed: number = 0;
 
-startGame(event: {playerName: string, email: string}) { 
+public state: 'intro' | 'game' = 'intro';
+
+public startGame(event: {playerName: string, email: string}) { 
+    this.state = 'game';
     this.playerName = event.playerName;
     this.email = event.email;
     this.gameStarted = true; 
 }
 
-exitGame (event:boolean) {
+public exitGame (event:boolean) {
+    this.state = 'intro';
     this.gameStarted = event
+  
 }
 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { GameComponent } from './game/game.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideRouter([
+      {path: 'intro', component:IntroComponent },
+      {path: 'game', component:GameComponent }
+    ])
+  ]
 };
